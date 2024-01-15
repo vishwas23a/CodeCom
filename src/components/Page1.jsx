8e40692dd1c206e3bfcb6aa60707e6a9da83a933
Vishwas Kumar Verma <vishwasverma23@gmail.com>
2024-01-15 15:53:33 +0530

    updated some things

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -41,17 +41,65 @@ export default function Page1() {
 </h1>
 
 <div className="aboutvision">
-<div className="vision1">
+<div className="visionimage">
+  <div className='visionimage1'>
   <h2>Our Vision</h2>
 
  
-  <p> * Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam expedita nihil quo magni voluptatibus sint minima saepe, quam ratione maxime, maiores veniam deserunt iste explicabo rerum! Doloribus, nisi saepe. Saepe.</p>
-  <p> * Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam expedita nihil quo magni voluptatibus sint minima saepe, quam ratione maxime, maiores veniam deserunt iste explicabo rerum! Doloribus, nisi saepe. Saepe.</p>
+  <p> <img src="./images/logo1.png" alt="" /> Lorem ipsum doloratione maxime, maiores veniam deserunt iste expli.</p>
+  <p>  <img src="./images/logo1.png" alt="" /> Lorem ipsum dolors veniam deserunt iste explicabo rerum! Doloribus</p>
+  
+  <p>  <img src="./images/logo1.png" alt="" /> Lorem ipsum dolors veniam deserunt iste explicabo rerum! Doloribus</p></div>
+  <div className="visionimage2">
+  <h2>Service</h2>
+
+ 
+  <p>  <img src="./images/logo1.png" alt="" /> Lorem ipsum doloratione maxime, maiores veniam deserunt iste expli</p>
+  <p>  <img src="./images/logo1.png" alt="" /> Lorem ipsum dolors veniam deserunt iste explicabo rerum! Doloribu.</p>
+  
+  <p>  <img src="./images/logo1.png" alt="" /> Lorem ipsum dolors veniam deserunt iste explicabo rerum! Doloribus,</p>
+
+ 
+</div>
+<div className='team'>
+<p>Hi I am a</p>
+<p className='front'> Frontend</p>
+<p>Developer</p>
+
+<h1 className='teamhead'>Vishwas</h1>
+</div>
+
 
 </div>
-<div className="visionimage">
-  <img src="" alt="" />
+
 </div>
+
+<div className="footer">
+<div className='product1'>
+
+  <h2>Resource</h2>
+  <a href='#'>Solution</a>
+  <a href='#'>Chat System</a>
+  <a href='#'>Notes</a>
+
+  <a href='#'>Video call</a>
+</div>
+<div className='product2'>
+
+  <h2>Target</h2>
+  <a href='#'>College</a>
+  <a href='#'>Coding clubs</a>
+  <a href='#'>Friends</a>
+  <a href='#'>Open Community</a>
+</div>
+<div className='product3'>
+
+  <h2>Contact Info</h2>
+  <a href='#'>vishwasverma23@gmail.com</a>
+  <a href='#'> 9506862942</a>
+ 
+</div>
+
 </div>
 </div>
 
