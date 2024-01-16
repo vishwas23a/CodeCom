f79aae055417a09e080d3cae78dcea1dbc5ad344
Vishwas Kumar Verma <vishwasverma23@gmail.com>
2024-01-17 01:43:56 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Page1 from './components/Page1'
-import Page2 from './components/Page2'
 import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
