1db925209f7583e3734ecef993845cdbbadfbd24
Vishwas Kumar Verma <vishwasverma23@gmail.com>
2024-01-17 01:37:40 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Page1 from './components/Page1'
 import Page2 from './components/Page2'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
 
   return (
     <>
-      
+      <SpeedInsights/>
 <Page1/>
 
     
